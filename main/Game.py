234e8932707afccbf3b5d25373460889901dafa6
Pol <Pol@gmail.com>
2021-03-22 11:44:07 +0100

    Equal scores implemented

--- main/Game.py
+++ main/Game.py
@@ -9,11 +9,16 @@ class Game:
         self.winner = ""
 
     def won_point(self, winner: Player):
-        self.winner = winner
+        self.winner.add_point()
+        self.winner = winner.name
 
     def get_score(self) -> str:
-        if self.winner == self.player1:
-            return "Fifteen-Love"
-        if self.winner == self.player2:
-            return "Fifteen-All"
-        return "Love-All"
+        if (self.player1.score - self.player2.score) == 0:
+            if self.player1.score == 0:
+                return "Love-All"
+            elif self.player1.score == 1:
+                return "Fifteen-All"
+            elif self.player1.score == 2:
+                return "Thirty-All"
+            else:
+                return "Deuce"
--- main/Player.py
+++ main/Player.py
@@ -5,3 +5,6 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
+
+    def add_point(self):
+        self.score += 1
